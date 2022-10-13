ba8579e7512189b25fa70e2cde2802ea5c10c8b1
Shingome <mishakkoles@gmai.com>
2022-10-13 16:36:52 +0300

    16:36 13.10.2022

--- Training/train.py
+++ Training/train.py
@@ -3,7 +3,8 @@ import tensorflow_datasets as tfds
 import numpy as np
 import os
 from keras.losses import SparseCategoricalCrossentropy
-from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from keras.utils.vis_utils import plot_model
 from PIL import Image
 
 
@@ -38,26 +39,28 @@ y_test = test['label']
 x_test = rebuild(x)
 
 model = keras.Sequential()
-model.add(Conv2D(32, (2, 2), input_shape=(20, 16, 1)))
-model.add(Activation('relu'))
+model.add(Conv2D(32, (2, 2), activation='relu', input_shape=(20, 16, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (2, 2)))
-model.add(Activation('relu'))
+model.add(Conv2D(32, (2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (2, 2)))
-model.add(Activation('relu'))
+model.add(Conv2D(64, (2, 2), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(36))
-model.add(Activation('softmax'))
+model.add(Dense(36, activation='softmax'))
 
-model.compile(optimizer='sgd', loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
+plot_model(model,
+           to_file="model_plot.png",
+           show_dtype=True,
+           show_shapes=True,
+           show_layer_names=True,
+           show_layer_activations=True)
+
+model.compile(optimizer='adam', loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
 
 model.fit(x, y, epochs=10, shuffle=True)
 
