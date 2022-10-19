f10decbc8cff7f6850facc7ffb9236314473a614
Shingome <mishakkoles@gmai.com>
2022-10-20 00:03:00 +0300

    00:02 20.10.2022

--- Training/train.py
+++ Training/train.py
@@ -25,43 +25,44 @@ def rebuild(x):
     return np.asarray(new_x)
 
 
-dataset, test = tfds.load('BinaryAlphaDigits', split=["train[:80%]", "train[80%:]"])
+if __name__ == "__main__":
+    dataset, test = tfds.load('BinaryAlphaDigits', split=["train[:80%]", "train[80%:]"])
 
-dataset = tfds.as_dataframe(dataset)
-test = tfds.as_dataframe(test)
+    dataset = tfds.as_dataframe(dataset)
+    test = tfds.as_dataframe(test)
 
-x = dataset['image']
-y = dataset['label']
-x = rebuild(x)
+    x = dataset['image']
+    y = dataset['label']
+    x = rebuild(x)
 
-x_test = test['image']
-y_test = test['label']
-x_test = rebuild(x)
+    x_test = test['image']
+    y_test = test['label']
+    x_test = rebuild(x)
 
-model = keras.Sequential()
-model.add(Conv2D(32, (2, 2), activation='relu', input_shape=(20, 16, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model = keras.Sequential()
+    model.add(Conv2D(32, (2, 2), activation='relu', input_shape=(20, 16, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (2, 2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (2, 2), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(36, activation='softmax'))
+    model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(36, activation='softmax'))
 
-plot_model(model,
-           to_file="model_plot.png",
-           show_dtype=True,
-           show_shapes=True,
-           show_layer_names=True,
-           show_layer_activations=True)
+    plot_model(model,
+               to_file="model_plot.png",
+               show_dtype=True,
+               show_shapes=True,
+               show_layer_names=True,
+               show_layer_activations=True)
 
-model.compile(optimizer='adam', loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
+    model.compile(optimizer='adam', loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
 
-model.fit(x, y, epochs=10, shuffle=True)
+    model.fit(x, y, epochs=50, shuffle=True)
 
-print(model.evaluate(x_test[:len(y_test)], y_test[:len(y_test)]))
+    print(model.evaluate(x_test[:len(y_test)], y_test[:len(y_test)]))
