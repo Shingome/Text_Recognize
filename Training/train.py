0ce746b5f8ef59af9e865333d80741a5c378c34f
Shingome <mishakkoles@gmai.com>
2022-10-24 21:49:55 +0300

    21:49 24.10.2022

--- Training/train.py
+++ Training/train.py
@@ -3,19 +3,12 @@ import tensorflow_datasets as tfds
 import numpy as np
 import os
 from keras.losses import SparseCategoricalCrossentropy
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, InputLayer
 from keras.utils.vis_utils import plot_model
-from PIL import Image
 
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-
-
-def show_image(image_array):
-    image_array.shape = (20, 16)
-    image = Image.fromarray(image_array)
-    image.show()
 
 
 def rebuild(x):
@@ -26,29 +19,27 @@ def rebuild(x):
 
 
 if __name__ == "__main__":
-    dataset, test = tfds.load('BinaryAlphaDigits', split=["train[:80%]", "train[80%:]"])
+    train_dataset, test_dataset = tfds.load('BinaryAlphaDigits', split=["train[:80%]", "train[80%:]"])
 
-    dataset = tfds.as_dataframe(dataset)
-    test = tfds.as_dataframe(test)
+    train_dataset = tfds.as_dataframe(train_dataset)
+    test_dataset = tfds.as_dataframe(test_dataset)
 
-    x = dataset['image']
-    y = dataset['label']
-    x = rebuild(x)
+    x_train = train_dataset['image']
+    y_train = train_dataset['label']
+    x_train = rebuild(x_train)
 
-    x_test = test['image']
-    y_test = test['label']
-    x_test = rebuild(x)
+    x_test = test_dataset['image']
+    y_test = test_dataset['label']
+    x_test = rebuild(x_test)
 
     model = keras.Sequential()
-    model.add(Conv2D(32, (2, 2), activation='relu', input_shape=(20, 16, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
+    model.add(InputLayer((20, 16, 1)))
     model.add(Conv2D(32, (2, 2), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-
+    model.add(Conv2D(32, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(64, (2, 2), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
@@ -63,6 +54,4 @@ if __name__ == "__main__":
 
     model.compile(optimizer='adam', loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
 
-    model.fit(x, y, epochs=50, shuffle=True)
-
-    print(model.evaluate(x_test[:len(y_test)], y_test[:len(y_test)]))
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=30, shuffle=True, batch_size=32)
